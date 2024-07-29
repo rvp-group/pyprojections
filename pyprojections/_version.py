870e6ff874553ffe6f4f89c444f1c8a041ed7ebe
Emanuele Giacomini <giacomini@diag.uniroma1.it>
2024-07-29 12:38:11 +0200

    small fixes

--- pyprojections/_version.py
+++ pyprojections/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.dev5+gf02fe0d.d20240729'
-__version_tuple__ = version_tuple = (0, 1, 'dev5', 'gf02fe0d.d20240729')
+__version__ = version = '0.1.dev6+g36f2ba5.d20240729'
+__version_tuple__ = version_tuple = (0, 1, 'dev6', 'g36f2ba5.d20240729')
